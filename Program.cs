b8d8ea938321ce334cb69a5f219b87a91880ca7f
Fred 17268332+FrederickRies@users.noreply.github.com <17268332+FrederickRies@users.noreply.github.com>
2021-06-09 00:09:35 +0200

    Fix compile time

--- Program.cs
+++ Program.cs
@@ -1,15 +1,11 @@
 using Api;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
-
-builder.Services.AddApiVersioning();
 
 // Create application
 using var app = builder.Build();
@@ -18,8 +14,6 @@ using var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 }
 
 app.UseHttpsRedirection();
